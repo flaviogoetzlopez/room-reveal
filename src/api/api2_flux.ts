9c0b4d50dfbabccee9556582c82ca8d5c90fd35e
Flavio <go69ceq@tum.de>
2025-11-30 02:14:33 +0100

    nicechanges

--- src/api/api2_flux.ts
+++ src/api/api2_flux.ts
@@ -1,10 +1,10 @@
 import { ProviderExoticComponent } from "react";
 const apiKey = process.env.BFL_API_KEY;
 
-async function createNewImageFromPrompt(prompt,) {
+async function createNewImageFromPrompt(prompt: string, input: string) {
     if (!apiKey) throw new Error("BFL_API_KEY not set");
 
-    const input_image = "https://mms.immowelt.de/d/b/e/e/dbee0711-46cd-401f-9225-330876b0fd84.jpg?ci_seal=a18ccaff317c9552f931a2d4afdca0a2187b2a8a&w=1024&h=576"
+    const input_image = input
     // fetch sends a POST with JSON body + required header
     // body contains prompt + the three input images + seed + output format
     const response = await fetch("https://api.bfl.ai/v1/flux-2-pro", {
